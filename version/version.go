9373cb7ba97e97a701d7fa89f663e3d8f9235239
Martin Grogan <martin.grogan@hashicorp.com>
2025-03-28 11:30:57 -0400

    version: add version file

---
+++ version/version.go
@@ -0,0 +1,13 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package version
+
+import "github.com/hashicorp/packer-plugin-sdk/version"
+
+var (
+	Version           = "1.0.2"
+	VersionPrerelease = "dev"
+	VersionMetadata   = ""
+	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
+)
