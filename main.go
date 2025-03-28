06d1b9953b0bfe5f7908ac2024d144c108de0e6c
Martin Grogan <martin.grogan@hashicorp.com>
2025-03-28 11:30:57 -0400

    version: add version file

--- main.go
+++ main.go
@@ -11,23 +11,9 @@ import (
 	"packer-plugin-hashicups/datasource/ingredients"
 	"packer-plugin-hashicups/post-processor/receipt"
 	"packer-plugin-hashicups/provisioner/toppings"
+	"packer-plugin-hashicups/version"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.2"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
 )
 
 func main() {
@@ -37,7 +23,7 @@ func main() {
 	pps.RegisterBuilder("order", new(order.Builder))
 	pps.RegisterProvisioner("toppings", new(toppings.Provisioner))
 	pps.RegisterPostProcessor("receipt", new(receipt.PostProcessor))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
